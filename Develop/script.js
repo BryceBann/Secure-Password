e57b8b8e568698fa8f8f76cc763747351942e95b
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-08-15 00:00:49 -0500

    major update in way of popup boxes interactive and chaning variables depending on input needs clean up and quality of life

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,93 @@
 // Assignment code here
-
+var passLength;
+var lower;
+var upper;
+var special;
+var number;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var passLength = 0;
-var lower = false;
-var upper = false;
-var numeric = false;
-var specialChar = false;
 
+const generatePassword = function(){ 
+  var passLength = window.prompt("Select characters amount between 8-128.")
+  if(passLength < 8){
+    window.alert("Not enough characters please try again.") 
+    generatePassword()
+  }else if(passLength > 128){
+    window.alert("Too many characters please try agian.")
+    generatePassword()
+  }else{
+    window.alert("Character amount set to " + passLength)
+  }
+  //self check function works
+  console.log(passLength)
 
-const generatePassword = function(){
-  var passLength = window.prompt("How many characters between 8-128")
-  if(prompt <! 8){
-    window.prompt("Not enough characters")
-  }else{}
-    
-    
   
+  var lower = window.prompt("Any lowercase characters?\n(Yes or No)")
+  lower = lower.toUpperCase();
+  if(lower ==="YES"){
+    window.alert("You chose: " + lower)
+    lower = true
+  }else if(lower === "NO"){
+    window.alert("You chose: " + lower)
+    lower = false
+  }else{
+    window.alert("Please check spelling")
+   //needs a return reset to top of lower for miss spelling
+  }
+ //self check log
+ console.log(lower)
+
+  var upper = window.prompt("Any uppercase characters\n(Yes or No)")
+  upper = upper.toUpperCase();
+  if(upper === "YES"){
+    window.alert("You chose: " + upper)
+    upper = true
+  }else if(upper === "NO"){
+    window.alert("You chose: " + upper)
+    upper = false
+  }else{
+    window.alert("Please check spelling.")
+    //needs return to reset to top of upper for miss spelling
+  }
+  //self check log
+  console.log(upper)
+
+  var special = window.prompt("Any special characters\n(Yes or No)")
+  special = special.toUpperCase();
+  if(special === "YES"){
+    window.alert("You chose: " + special)
+    special = true
+  }else if(special === "NO"){
+    window.alert("You chose: " + special)
+    special = false
+  }else{
+    window.alert("Please check spelling.")
+    //needs return to reset to top of special for miss spelling
+  }
+  //self check log
+  console.log(special)
+
+  var number = window.prompt("Any numerical characters\n(Yes or No)")
+  number = number.toUpperCase();
+  if(number === "YES"){
+    window.alert("You chose: " + number)
+    number = true
+  }else if(number=== "NO"){
+    window.alert("You chose: " + number)
+    number = false
+  }else{
+    window.alert("Please check spelling.")
+    //needs return to reset to top of number for miss spelling 
+  }
+  //self check log
+  console.log(number)
+
+
 
 }
-console.log(passLength)
+
+
 
 // Write password to the #password input
 function writePassword() {
